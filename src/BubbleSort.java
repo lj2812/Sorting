cab0d3ddf3a6456ec5d2255ff6926cfb156a6041
lj7038 <lj7038@CSELAB03-21.bennett.edu.in>
2019-02-12 11:53:51 +0530

    merge branch commit for sorting

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,9 +1,35 @@
 
+
 public class BubbleSort {
+	void bubbleSorting(int array[])
+	 {
+	     int num = array.length;
+	     for (int i = 0; i < num-1; i++)
+	         for (int j = 0; j < num-i-1; j++)
+	             if (array[j] > array[j+1])
+	             {
+	                 
+	                 int temp = array[j];
+	                 array[j] = array[j+1];
+	                 array[j+1] = temp;
+	             }
+	 }	 
+
+	 void printArray(int array[])
+	 {
+	     int n = array.length;
+	     for (int i=0; i<n; ++i)
+	         System.out.print(array[i] + " ");
+	     System.out.println();
+	 }
+
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BubbleSort ob = new BubbleSort();
+	     int array[] = {80, 20, 15, 21, 55, 11, 70};
+	     ob.bubbleSorting(array);
+	     System.out.println("Sorted array");
+	     ob.printArray(array);
 
 	}
-
 }
